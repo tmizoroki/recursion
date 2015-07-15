5f8026965a494985801e86ed4325b721a34befc8
Tomio Mizoroki <tmizoroki@gmail.com>
2015-07-14 21:17:08 -0700

    Implement stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,22 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  var resultString = '';
-
-  var wrapQuotes = function(item) {
-  	return '\"' + item + '\"';
-  };
-
-  var findObjs = function(obj) {
-  	if (typeof obj !== 'object' || obj === null) {
-  		if (typeof obj === 'string') {
-		  	resultString += wrapQuotes(obj);
-  		} else {
-	  		resultString += obj;
-	  	}
-  	}
-
-  }(obj);
-
-  return resultString;
+	var resultString = '';
+	var wrapQuotes = function(item) {
+		return '\"' + item + '\"';
+	};
+	var findObjs = function(obj) {
+		if (typeof obj !== 'object' || obj === null) {
+			if (typeof obj === 'string') {
+				resultString += wrapQuotes(obj);
+			} else if (typeof obj === 'undefined' || typeof obj === 'function') {
+				resultString = undefined;
+			} else {
+				resultString += obj;
+			}
+		}
+		if (Array.isArray(obj)) {
+			resultString += '[';
+			_.each(obj, function(item, i) {
+				findObjs(item);
+				if (i !== obj.length - 1) {
+					resultString += ',';
+				}
+			});
+			resultString += ']';
+		} else if (Object.prototype.toString.call(obj) === '[object Object]') {
+			resultString += '{';
+			_.each(obj, function(item, i) {
+				if (!(item === undefined || typeof item === 'function')) {
+					resultString += wrapQuotes(i) + ':';
+					findObjs(item);
+					if (i !== Object.keys(obj)[Object.keys(obj).length - 1]) {
+						resultString += ',';
+					}
+				}
+			});
+			resultString += '}';
+		}
+	};
+	findObjs(obj);
+	return resultString;
 };
